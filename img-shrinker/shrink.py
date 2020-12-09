3069b63a07a03148a60ddce5577851eefcc3b28a
Garett <garettmd@gmail.com>
2020-12-09 14:11:24 -0500

    working

--- img-shrinker/shrink.py
+++ img-shrinker/shrink.py
@@ -14,7 +14,13 @@ def shrinker(files, size):
 
     for file in files:
         proc = Popen(
-            [cmd, file, "-define", "jpeg:extent={0}".format(size), "{0}_shrunk.jpg".format(file)],
+            [
+                cmd,
+                file,
+                "-define",
+                "jpeg:extent={0}".format(size),
+                "{0}_shrunk.jpg".format(file),
+            ],
             stdout=PIPE,
             stderr=PIPE,
         )
@@ -24,12 +30,16 @@ def shrinker(files, size):
 
 @Gooey()
 def main():
-    parser = GooeyParser(description='Shrink images!')
-    parser.add_argument('input', help='Files to shrink', widget='FileChooser')
-    parser.add_argument('size', help='Max size to shrink images to', choices=['1kb', '500kb', '1mb', '10mb', '100mb'])
+    parser = GooeyParser(description="Shrink images!")
+    parser.add_argument("input", help="Files to shrink", widget="FileChooser")
+    parser.add_argument(
+        "size",
+        help="Max size to shrink images to",
+        choices=["1kb", "500kb", "1mb", "10mb", "100mb"],
+    )
     args = parser.parse_args()
     shrinker(args.input, args.size)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
